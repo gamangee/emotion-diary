6c838d42ad6f7b5a9ab4b6ab274286ce27ffbd22
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-07-08 03:02:34 +0900

    :tada: Initial Setting
    초기세팅 및 기본 라이브러리 설치

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import macro from "vite-plugin-babel-macros";
+
+export default defineConfig({
+  plugins: [react(), macro()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true, // 개발자 도구에 소스맵 생성 설정
+  },
+});
